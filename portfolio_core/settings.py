b6cf0b0b36a8afd4cfa3d03d21ce05cf862f96e9
Amir Ghadimi <Sraldon24@gmail.com>
2026-02-03 15:04:50 -0500

    Add LOGGING to capture 500 errors in stdout

--- portfolio_core/settings.py
+++ portfolio_core/settings.py
@@ -189,3 +189,29 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
