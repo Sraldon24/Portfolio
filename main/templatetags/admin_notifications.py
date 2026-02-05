4f0e5e4b8b9c123fa8d9481be68e7d211e9e719a
Amir Ghadimi <Sraldon24@gmail.com>
2026-02-05 13:36:36 -0500

    Fix admin: Implement missing get_pending_counts template tag

--- main/templatetags/admin_notifications.py
+++ main/templatetags/admin_notifications.py
@@ -1,10 +1,12 @@
 from django import template
-from main.models import Testimonial, ContactMessage
+from main.models import Testimonial
 
 register = template.Library()
 
-def pending_testimonials_count(request):
+@register.simple_tag
+def get_pending_counts():
+    """
+    Returns the count of pending items (currently only unapproved testimonials).
+    This is used in the admin sidebar or header to show notification badges.
+    """
     return Testimonial.objects.filter(is_approved=False).count()
-
-def total_messages_count(request):
-    return ContactMessage.objects.count()
